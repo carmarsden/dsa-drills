332ad635295a8021a99df6267adc151c03c3b269
Caroline Marsden <car.marsden@gmail.com>
2019-09-07 17:53:13 -0400

    Binary Search Tree drills 7-9

--- binary-search-trees/bst-drills.js
+++ binary-search-trees/bst-drills.js
@@ -82,4 +82,98 @@ function isBST(tree) {
     return (leftOk && rightOk && isBST(tree.left) && isBST(tree.right))
 }
 
-console.log(isBST(drill3nums()));
+
+// DRILL 7
+
+function reverseOrderTraverse(node, fn) {
+    // reverse order traversal:
+    // base case: if there's no node, return
+    if (!node) {
+        return;
+    }
+
+    // otherwise, search (in order) right subtree, node, left subtree
+    // when you evaluate the node, evaluate via callback function
+    // return value from either the searching or the callback function
+
+    return reverseOrderTraverse(node.right, fn) 
+        || fn(node)
+        || reverseOrderTraverse(node.left, fn)
+}
+
+function kLargest(tree, k, count=0) {
+    // result will be the value that reverseOrderTraverse spits out
+    let result = reverseOrderTraverse(tree, function(node) {
+        // when you evaluate the node, increase the count
+        count++;
+        console.log(`just looked at ${node.value}; current count ${count}`)
+        // if we've reached the desired k, return the current node value to reverseOrderTraverse (so it can return it back to this result variable)
+        if (count === k) {
+            return node.value
+        }
+    });
+
+    return result;
+}
+
+// console.log(kLargest(drill3nums(), 3))
+
+// DRILL 8
+
+function isBalanced(tree) {
+    // use findHeight algorithm to find the height of each of the two subtrees
+
+    let leftHeight = 0;
+    let rightHeight = 0;
+
+    if (tree.left) {
+        leftHeight = findHeight(tree.left)
+    }
+
+    if (tree.right) {
+        rightHeight = findHeight(tree.right)
+    }
+
+    // difference between left & right must be no more than 1 if isBalanced = true
+    return (Math.abs(leftHeight - rightHeight) <= 1)
+}
+
+function createBalanced() {
+    const BST = new BinarySearchTree();
+
+    BST.insert(4, 4);
+    BST.insert(1, 1);
+    BST.insert(6, 6);
+    BST.insert(9, 9);
+    BST.insert(2, 2);
+    BST.insert(5, 5);
+    BST.insert(7, 7);
+
+    return BST;
+}
+
+
+// DRILL 9
+
+function identicalTrees(arr1, arr2) {
+    // BST's will end up sorted, so we can just sort and compare the two input arrays. If they are the same, BST's will end up the same
+
+    // Sort arrays
+    arr1 = arr1.sort();
+    arr2 = arr2.sort();
+
+    // Compare arrays
+    if (arr1.length !== arr2.length) {
+        return false
+    }
+
+    for (i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false
+        }
+    }
+
+    return true;
+}
+
+console.log(identicalTrees([3, 5, 4, 6, 7, 0, 2], [3, 1, 5, 2, 4, 6, 0]))
--- binary-search-trees/bst-drills.md
+++ binary-search-trees/bst-drills.md
@@ -1,14 +1,18 @@
 ## 1. Draw a BST
 * Given the data 3,1,4,6,9,2,5,7, if you were to insert this into an empty binary search tree, what would the tree look like? (Draw the tree, no coding needed here.)
-    * ![BST with numbers](drill1-numbers.png)
+
+![BST with numbers](drill1-numbers.png)
+
 * Draw the BST with the keys - E A S Y Q U E S T I O N
-    * ![BST with letters](drill1-letters.png)
+
+![BST with letters](drill1-letters.png)
 
 ## 2. Remove the root
 Show how the above trees would look like if you deleted the root of each tree. (Draw the trees, no coding needed here.)
 
-* ![BST with numbers](drill2-numbers.png)
-* ![BST with letters](drill2-letters.png)
+![BST with numbers](drill2-numbers.png)
+
+![BST with letters](drill2-letters.png)
 
 ## 3. Create a BST class
 Walk through the binary search tree code in the curriculum and understand it well. Then write a BinarySearchTree class with its core functions (insert(), remove(), find()) from scratch.
@@ -48,9 +52,14 @@ Write an algorithm to check whether an arbitrary binary tree is a binary search 
 ## 7. 3rd largest node
 Write an algorithm to find the 3rd largest node in a binary search tree.
 
+**See bst-drills.js: kLargest**
+
 ## 8. Balanced BST
 Write an algorithm that checks if a BST is balanced (i.e., a tree where no 2 leaves differ in distance from the root by more than 1).
+
+**See bst-drills.js: isBalanced**
 
 ## 9. Are they the same BSTs?
 You are given two arrays which represent two sequences of keys that are used to create two binary search trees. Write a program that will tell whether the two BSTs will be identical or not without actually constructing the tree. You may use another data structure such as an array or a linked list but don't construct the BST. What is the time complexity of your algorithm? E.g., 3, 5, 4, 6, 1, 0, 2 and 3, 1, 5, 2, 4, 6, 0 are two sequences of arrays but will create the exact same BSTs and your program should return true.
 
+**See bst-drills.js: identicalTrees**
